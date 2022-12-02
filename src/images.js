18e871050d4bbe75cb9d6da6da4324d07572cbe2
NefisaB <nefisa.hc@gmail.com>
2022-12-02 10:21:11 +0100

    Add menu page.Change styles.

---
+++ src/images.js
@@ -0,0 +1,44 @@
+const images = [
+    {
+        title: "Cookies To Go",
+        url: "https://images.unsplash.com/photo-1622467827417-bbe2237067a9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80",
+        author: "Conor Brown"
+    },
+    {
+        title: "Macarons",
+        url: "https://images.unsplash.com/photo-1548848221-0c2e497ed557?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80",
+        author: "Mockaroon"
+    },
+    {
+        title: "Choco Chip Cookies",
+        url: "https://images.unsplash.com/photo-1628559087259-8a472928f47a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1650&q=80",
+        author: "Lisa Hanly"
+    },
+    {
+        title: "Linzer Cookies",
+        url: "https://images.unsplash.com/photo-1594236114270-f5feb79f52a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1522&q=80",
+        author: "amirali mirhashemian"
+    },
+    {
+        title: "Double Chocolate",
+        url: "https://images.unsplash.com/photo-1519682214708-973477a2529a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+        author: "charlesdeluvio"
+    },
+    {
+        title: "Ice cream brownies",
+        url: "https://images.unsplash.com/photo-1624353365286-3f8d62daad51?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+        author: "Shivansh Sethi"
+    },
+    {
+        title: "Cocoa Brownies",
+        url: "https://images.unsplash.com/photo-1612886623977-192b32a0d789?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80",
+        author: "Aneta Voborilova"
+    },
+    {
+        title: "Glass Of Milk",
+        url: "https://images.unsplash.com/photo-1567011345445-fd175f248019?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80",
+        author: "NordWood Themes"
+    }
+];
+
+export default images;
--- src/menu-page.js
+++ src/menu-page.js
@@ -1,5 +1,6 @@
 import basicLayout from "./basiclayout";
 import './style.css';
+import images from "./images";
 
 basicLayout();
 
@@ -9,6 +10,24 @@ function loadMenuPage() {
     const heading1 = document.createElement("h1");
     heading1.textContent = "Menu";
     mainSection.appendChild(heading1);
+
+    const menuList = document.createElement("div");
+    menuList.classList.add("menu-list");
+    for (let image of images) {
+        const card = document.createElement("div");
+        card.classList.add("image-card");
+        const imageEl = document.createElement("img");
+        imageEl.src = image.url;
+        imageEl.setAttribute("title", `Create by ${image.author} on Unsplah`);
+        const title = document.createElement("h3");
+        title.textContent = image.title;
+        card.appendChild(imageEl);
+        card.appendChild(title);
+        menuList.appendChild(card);
+    }
+
+    mainSection.appendChild(menuList);
+
 }
 
 export default loadMenuPage;
